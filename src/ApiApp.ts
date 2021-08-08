76d3147831216264aa5c63ede01f1d3b2e5c3abb
Paulappz <pauloluyege@gmail.com>
2021-08-08 11:17:03 +0100

    Set up API HTTP server

---
+++ src/ApiApp.ts
@@ -0,0 +1,40 @@
+import express, { Application } from 'express';
+
+
+// Import Router
+
+
+class ApiApp {
+  private application: Application;
+
+  constructor() {
+    this.application = express();
+    this.setupGlobalMiddleware();
+    this.setupRouters();
+  }
+
+  start(port: string | number = 3000) {
+    return this.application.listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`listening on port ${port}`);
+    });
+  }
+
+  getApplication() {
+    return this.application;
+  }
+
+  private setupGlobalMiddleware() {
+    this.application.use(express.json());
+  }
+
+  private setupRouters() {
+    this.application.get('/', (_, res) => {
+      res.json({ message: 'Welcome to our service!'});
+    });
+
+
+  }
+}
+
+export default new ApiApp();
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+// Import API application
+import ApiApp from './ApiApp';
+
+// Load Config
+dotenv.config();
+
+
+//Connect mongoose DB
+(async () => {
+  await mongoose.connect(process.env.MONGO_URI as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
+  // Start Application
+  ApiApp.start(process.env.PORT);
+})();
