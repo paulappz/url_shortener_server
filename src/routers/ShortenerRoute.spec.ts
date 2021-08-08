8b649af8ac95f34cf19b0961a9d0737c5b54b0d2
Paulappz <pauloluyege@gmail.com>
2021-08-08 13:03:10 +0100

    Write test for routes

--- src/routers/ShortenerRoute.spec.ts
+++ src/routers/ShortenerRoute.spec.ts
@@ -1,36 +1,89 @@
-import { Router } from 'express';
+import express, { Application } from 'express';
+import { MockedObject, mocked } from 'ts-jest/dist/utils/testing';
+import supertest from 'supertest';
 
+import ShortenerRoute from './ShortenerRoute';
 import UrlController from '../controllers/ShortenerController';
 
-class ShortenerRoute {
-    private router: Router;
+jest.mock('../controllers/ShortenerController');
 
-    constructor() {
-        this.router = Router();
-        this.setupRouter();
-    }
+describe('ShortenerRoute', () => {
+  const shortId = 'shortId';
+  const list = 'list';
 
-    private setupRouter() {
-        this.router.get('/:shortId', async (req, res) => {
-            await UrlController.redirectUrl(req, res);
-        });
+  const expectedResponse = expect.anything();
 
-        this.router.post('/api/encode', async (req, res) => {
-            await UrlController.shortenUrl(req, res);
-        });
+  let app: Application;
+  let request: supertest.SuperTest<supertest.Test>;
+  let UrlControllerMock: MockedObject<typeof UrlController>;
 
-        this.router.get('/api/list', async (req, res) => {
-            await UrlController.getUrls(req, res);
-        });
+  beforeEach(() => {
+    UrlControllerMock = mocked(UrlController);
+    UrlControllerMock.redirectUrl.mockImplementation(async (_, res) => {
+      return res.json({});
+    });
+    UrlControllerMock.shortenUrl.mockImplementation(async (_, res) => {
+      return res.json({});
+    });
 
-        this.router.post('/api/decode', async (req, res) => {
-            await UrlController.decodeUrl(req, res);
-        });
-    }
+    UrlControllerMock.getUrls.mockImplementation(async (_, res) => {
+      return res.json({});
+    });
 
-    getRouter() {
-        return this.router;
-    }
-}
+    UrlControllerMock.decodeUrl.mockImplementation(async (_, res) => {
+      return res.json({});
+    });
 
-export default new ShortenerRoute();
+    app = express();
+    app.use(express.json());
+    app.use('/', ShortenerRoute.getRouter());
+    request = supertest(app);
+  });
+
+  it('should call UrlController.redirectUrl function when GET /:shortId', async () => {
+    await request.get(`/${shortId}`);
+
+    expect(UrlControllerMock.redirectUrl)
+      .toHaveBeenCalledWith(
+        expect.objectContaining({
+          params: { shortId },
+        }),
+        expectedResponse,
+      );
+  });
+
+  it('should call UrlController.decodeUrl function when POST /', async () => {
+    const body = { shortId: 'short-id' };
+
+    await request.post(`/api/decode`).send(body);
+
+    expect(UrlControllerMock.decodeUrl)
+      .toHaveBeenCalledWith(
+        expect.objectContaining({
+          body,
+        }), expectedResponse,
+      );
+  });
+
+
+  it('should call UrlController.shortenUrl function when POST /', async () => {
+    const body = { url: 'too-long-url' };
+
+    await request.post(`/api/encode`).send(body);
+
+    expect(UrlControllerMock.shortenUrl)
+      .toHaveBeenCalledWith(
+        expect.objectContaining({
+          body,
+        }), expectedResponse,
+      );
+  });
+
+  it('should call UrlController.getUrls function when GET /getUrls', async () => {
+    await request.get(`/api/list`);
+
+    expect(UrlControllerMock.getUrls)
+      .toHaveBeenCalled();
+  });
+
+});
