08999ce83eec6aa0c34474f69217e60c4279668b
Paulappz <pauloluyege@gmail.com>
2021-08-08 11:42:38 +0100

    Create controller class

---
+++ src/controllers/ShortenerController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import IdGenerator from 'shortid';
+import Validator from 'validator';
+
+import UrlModel from '../models/UrlModel';
+
+class ShortenerController {
+
+
+}
+
+export default new ShortenerController();
