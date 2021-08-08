95d41b10922622e6d75d3fa4101a2d196cc5a0b0
Paulappz <pauloluyege@gmail.com>
2021-08-08 13:01:47 +0100

    Write test for routes

---
+++ src/routers/ShortenerRoute.spec.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+
+import UrlController from '../controllers/ShortenerController';
+
+class ShortenerRoute {
+    private router: Router;
+
+    constructor() {
+        this.router = Router();
+        this.setupRouter();
+    }
+
+    private setupRouter() {
+        this.router.get('/:shortId', async (req, res) => {
+            await UrlController.redirectUrl(req, res);
+        });
+
+        this.router.post('/api/encode', async (req, res) => {
+            await UrlController.shortenUrl(req, res);
+        });
+
+        this.router.get('/api/list', async (req, res) => {
+            await UrlController.getUrls(req, res);
+        });
+
+        this.router.post('/api/decode', async (req, res) => {
+            await UrlController.decodeUrl(req, res);
+        });
+    }
+
+    getRouter() {
+        return this.router;
+    }
+}
+
+export default new ShortenerRoute();
